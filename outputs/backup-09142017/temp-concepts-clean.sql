119181547d0be583f2a76c49b7d3865bd1377ff3
eric-chou <echou53@gmail.com>
2017-09-22 15:36:35 -0400

    added relationships, ancestors to clean sql script

--- outputs/backup-09142017/temp-concepts-clean.sql
+++ outputs/backup-09142017/temp-concepts-clean.sql
@@ -2,3 +2,5 @@ DELETE FROM public.concept WHERE concept_id BETWEEN -9999999 AND -7000000;
 DELETE FROM public.vocabulary WHERE vocabulary_concept_id BETWEEN -9999999 AND -7000000;
 DELETE FROM public.domain WHERE domain_concept_id BETWEEN -9999999 AND -7000000;
 DELETE FROM public.concept_class WHERE concept_class_concept_id BETWEEN -9999999 AND -7000000;
+DELETE FROM public.concept_relationship WHERE (concept_id_1 BETWEEN -9999999 AND -7000000) AND (concept_id_2 BETWEEN -9999999 AND -7000000);
+DELETE FROM public.concept_ancestor WHERE (ancestor_concept_id BETWEEN -9999999 AND -7000000) AND (descendant_concept_id BETWEEN -9999999 AND -7000000);
